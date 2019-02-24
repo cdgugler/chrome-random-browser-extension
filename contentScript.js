1cd4892440d14279c62b6db286276a6ae7830599
Cory Gugler <cory.gugler@gmail.com>
2019-02-24 16:13:19 -0600

    Add todos

--- contentScript.js
+++ contentScript.js
@@ -2,6 +2,8 @@ function navigateToRandomUrl() {
     let links = document.querySelectorAll('a:not([href^="#"])');
     let link = links[random(links.length)];
 
+    // TODO: remove links with targets or update location instead
+    // TODO: option to randomly start back at new seed url
     link.click();
 }
 
